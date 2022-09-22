91bcc9b29d4a7d5673a232dabe47c6840d750f4e
Jasmine Lopez <jasmine@adadevelopersacademy.org>
2022-09-22 11:49:13 -0700

    added super to text

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import cowsay
 
 
-cowsay.cow("Have a great day! Cheetahs is great! All instructors and classmates are super awesome! ")
+cowsay.cow("Have a SUPER day! Cheetahs is great! All instructors and classmates are super awesome! ")
 
 cowsay.dragon("RAWRRR RAWRRRRRR")
 
-cowsay.cow("It's going to be great!! :) ")
+cowsay.cow("It's going to be super great!! :) ")
 
