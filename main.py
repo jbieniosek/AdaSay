07be6cd98f2b92d42431261e1d89c9fc71b8af98
Jasmine Lopez <jasmine@adadevelopersacademy.org>
2022-09-22 11:26:48 -0700

    adding another cow

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ import cowsay
 cowsay.cow("Have a great day! Cheetahs is great! All instructors and classmates are super awesome! ")
 
 cowsay.dragon("RAWRRR RAWRRRRRR")
+
+cowsay.cow("It's going to be great!! :) ")
+
